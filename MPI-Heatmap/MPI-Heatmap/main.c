07cd3723f43aa716420c6e5a7848e9c9db0bb0a4
Jonas <jonas@jonass-mbp.railnet.train>
2014-01-18 12:41:09 +0100

    created scheme. imported original c version.

--- MPI-Heatmap/MPI-Heatmap/main.c
+++ MPI-Heatmap/MPI-Heatmap/main.c
@@ -1,18 +1,21 @@
-//
-//  main.c
-//  MPI-Heatmap
-//
-//  Created by Jonas on 18/01/14.
-//  Copyright (c) 2014 Jonas. All rights reserved.
-//
+#include "heatmap.h"
+#include "parse_coordinates.c"
+#include "parse_and_count_hotspots.c"
+#include "generate_and_run_heatmap.c"
+#include "generate_output.c"
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[])
-{
-
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+int main(int argc, const char * argv[]){
+    int width = atoi(argv[1]);
+    int height = atoi(argv[2]);
+    int rounds = atoi(argv[3]) + 1; //one extra round
+    int number_of_coords = 0;
+    int* coords = NULL;
+    int* hotspots = NULL;
+    
+    if (argc == 6) number_of_coords = parse_coordinates(argv[5], &coords);
+    int number_of_hotspots = parse_and_count_hotspots(argv[4], &hotspots);
+    double* heatmap = generate_and_run_heatmap(width, height, rounds, number_of_hotspots, hotspots);
+    generate_output(argc, number_of_coords, coords, width, height, heatmap);
+    
+    return 0;}
 
