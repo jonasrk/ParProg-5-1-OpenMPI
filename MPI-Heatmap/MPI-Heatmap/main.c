cbc6ac46882251e35112ea41f57824b0bad091c2
Jonas <jonas@jonass-mbp.railnet.train>
2014-01-18 12:33:19 +0100

    initialized xcode project

---
+++ MPI-Heatmap/MPI-Heatmap/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MPI-Heatmap
+//
+//  Created by Jonas on 18/01/14.
+//  Copyright (c) 2014 Jonas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
