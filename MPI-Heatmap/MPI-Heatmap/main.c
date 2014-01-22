1dd35474e582a5a5ddf6b606750d64dc31aa0c2f
Jonas <jonas@wlan-141-23-76-151.tubit.tu-berlin.de>
2014-01-22 18:48:48 +0100

    hello world is now working with both the makefile and from within xcode.

--- MPI-Heatmap/MPI-Heatmap/main.c
+++ MPI-Heatmap/MPI-Heatmap/main.c
@@ -13,6 +13,8 @@ int main(int argc, const char * argv[]){
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     
+    if (world_rank == 0) {
+        
     // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
@@ -22,21 +24,24 @@ int main(int argc, const char * argv[]){
     printf("Hello world from processor %s, rank %d"
            " out of %d processors\n",
            processor_name, world_rank, world_size);
+        
+    }
     
     // Finalize the MPI environment.
     MPI_Finalize();
     
-    int width = atoi(argv[1]);
-    int height = atoi(argv[2]);
-    int rounds = atoi(argv[3]) + 1; //one extra round
-    int number_of_coords = 0;
-    int* coords = NULL;
-    int* hotspots = NULL;
-    
-    if (argc == 6) number_of_coords = parse_coordinates(argv[5], &coords);
-    int number_of_hotspots = parse_and_count_hotspots(argv[4], &hotspots);
-    double* heatmap = generate_and_run_heatmap(width, height, rounds, number_of_hotspots, hotspots);
-    generate_output(argc, number_of_coords, coords, width, height, heatmap);
-    
-    return 0;}
+//    int width = atoi(argv[1]);
+//    int height = atoi(argv[2]);
+//    int rounds = atoi(argv[3]) + 1; //one extra round
+//    int number_of_coords = 0;
+//    int* coords = NULL;
+//    int* hotspots = NULL;
+//    
+//    if (argc == 6) number_of_coords = parse_coordinates(argv[5], &coords);
+//    int number_of_hotspots = parse_and_count_hotspots(argv[4], &hotspots);
+//    double* heatmap = generate_and_run_heatmap(width, height, rounds, number_of_hotspots, hotspots);
+//    generate_output(argc, number_of_coords, coords, width, height, heatmap);
+//    
+//    return 0;
+}
 
