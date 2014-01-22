bbc45ab35f6b67ab61b60e212cb4846c7a867e2a
Jonas <jonas@wlan-141-23-76-151.tubit.tu-berlin.de>
2014-01-22 19:10:14 +0100

    simple assignment example works from commandline and xcode

--- MPI-Heatmap/MPI-Heatmap/main.c
+++ MPI-Heatmap/MPI-Heatmap/main.c
@@ -15,33 +15,21 @@ int main(int argc, const char * argv[]){
     
     if (world_rank == 0) {
         
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
-    
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d"
-           " out of %d processors\n",
-           processor_name, world_rank, world_size);
+        int width = atoi(argv[1]);
+        int height = atoi(argv[2]);
+        int rounds = atoi(argv[3]) + 1; //one extra round
+        int number_of_coords = 0;
+        int* coords = NULL;
+        int* hotspots = NULL;
+        
+        if (argc == 6) number_of_coords = parse_coordinates(argv[5], &coords);
+        int number_of_hotspots = parse_and_count_hotspots(argv[4], &hotspots);
+        double* heatmap = generate_and_run_heatmap(width, height, rounds, number_of_hotspots, hotspots);
+        generate_output(argc, number_of_coords, coords, width, height, heatmap);
         
     }
     
     // Finalize the MPI environment.
     MPI_Finalize();
-    
-//    int width = atoi(argv[1]);
-//    int height = atoi(argv[2]);
-//    int rounds = atoi(argv[3]) + 1; //one extra round
-//    int number_of_coords = 0;
-//    int* coords = NULL;
-//    int* hotspots = NULL;
-//    
-//    if (argc == 6) number_of_coords = parse_coordinates(argv[5], &coords);
-//    int number_of_hotspots = parse_and_count_hotspots(argv[4], &hotspots);
-//    double* heatmap = generate_and_run_heatmap(width, height, rounds, number_of_hotspots, hotspots);
-//    generate_output(argc, number_of_coords, coords, width, height, heatmap);
-//    
-//    return 0;
 }
 
